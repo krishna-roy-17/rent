cc13ea3002d2b7c41e15324e18d7ff7214aa6259
krishna roy <roykrishna9819.in>
2025-07-04 21:41:17 +0545

    rental system

---
+++ index.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agriculture Equipment Rental</title>
+    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/> -->
+<!-- contact page -->
+<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" /> -->
+<link rel="stylesheet" href="./css/index_style.css"/>
+    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.4.2/css/fontawesome.min.css" integrity="sha384-BY+fdrpOd3gfeRvTSMT+VUZmA728cfF9Z2G42xpaRkUGu2i3DyzpTURDo5A6CaLK" crossorigin="anonymous"> -->
+</head>
+<body>
+    <div class="b1">
+         <div class="contact">
+            <span>📞 7984120198</span> 
+            <span>📧 info@gmail.com</span>
+            <div class="auth-links">
+                <a href="#">Login</a>
+                <a href="#">Signup</a>
+                <a href="#">Admin</a>
+            </div>
+        </div>
+    <div class="navbar">
+        <!-- <h1></h1> -->
+        <a href="index.html" class="logo"><img src="./image/logo.jpg" alt="logo"></img></a>
+        <p>Agriculture Equipment Rental Platform </p>
+        <ul>
+            <li><a href="index.php">HOME</a></li>
+            <li><a href="#">CATEGORY</a>
+            <div class="dropdown">
+                <ul>
+                    <li><a href="#">Lawn and Gardening</a></li>
+                    <li><a href="#">Hand Tools</a></li>
+                    <li><a href="#">Earth Auger</a></li>
+                    <li><a href="#">Harvesters</a></li>
+                    <li><a href="#">Sprayers</a></li>
+                    <li><a href="#">Post Harvest</a></li>
+                    <li><a href="#">Water Pump</a></li>
+                    <li><a href="#">Lightening and Electric</a></li>
+                    <li><a href="#">Brush Cutters</a></li>
+                </ul>
+            </div>
+
+            </li>
+            <li><a href="/facility/facility.html">ABOUT</a></li>
+            <li><a href="/blog/blog.html">SHOP PAGE</a></li>
+            <li><a href="/contact/contactus.html">CONTACT US</a></li>
+            <!-- <li id="regs"><a href="/registration/form.html">Registration</a></li> -->
+
+
+        </ul>
+    </div>
+    <div class="quotes">
+    <h1>WE SERVE FRESH FRUITS & VEGETABLES</h1>
+    <p>WE DELIVER OQGANIC FRUITS & VEGETABLES</p>
+    <a class="details" href="#about">VIEW DETAILS</a>
+    <!-- <button class="vcourse">view courses</button> -->
+    </div>
+    </div>
+
+<!-- 
+      <section class="features">
+    <div class="feature">
+      <div class="icon" style="background-color: #e9c4e9;">
+        <img src="./image/truck-icon.png" alt="Free Shipping" />
+      </div>
+      <h3>FREE SHIPPING</h3>
+      <p>ON ORDER OVER $100</p>
+    </div>
+    <div class="feature">
+      <div class="icon" style="background-color: #d9c99e;">
+        <img src="./image/fresh-icon.png" alt="Always Fresh" />
+      </div>
+      <h3>ALWAYS FRESH</h3>
+      <p>PRODUCT WELL PACKAGE</p>
+    </div>
+    <div class="feature">
+      <div class="icon" style="background-color: #b6dce0;">
+        <img src="./image/quality-icon.png" alt="Superior Quality" />
+      </div>
+      <h3>SUPERIOR QUALITY</h3>
+      <p>QUALITY PRODUCTS</p>
+    </div>
+    <div class="feature">
+      <div class="icon" style="background-color: #d8d29e;">
+        <img src="./image/support-icon.png" alt="Support" />
+      </div>
+      <h3>SUPPORT</h3>
+      <p>24/7 SUPPORT</p>
+    </div>
+  </section> -->
+
+  <section class="fpage">
+        <div class="Features_page">
+            <h1>FEATURES</h1>
+            <p>Our features and Supports..</p>  
+        </div>
+        <div class="list_features">
+            <div class="list_of_second_div">
+                <div class="fc_image" id="truck_image">
+                    <img src="./image/truck-icon.png" alt="">
+                </div>
+                <div class="text">
+                    <h2>FREE SHIPPING</h2>
+                    <p>ON ORDER OVER $100</p>
+                </div>
+            </div>
+            <div class="list_of_second_div">
+                <div class="fc_image" id="fresh_image">
+                    <img src="./image/fresh-icon.png" alt="">
+                </div>
+                <div class="text">
+                    <h2>ALWAYS FRESH</h2>
+                    <p>PRODUCT WILL PURCHASE</p>
+                </div>
+            </div>
+            <div class="list_of_second_div">
+                <div class="fc_image" id="quality_image">
+                    <img src="./image/quality-icon.png" alt="">
+                </div>
+                <div  class="text">
+                     <h2>SUPERIOR QUALITY</h2>
+                    <p>QUALITY PRODUCTS</p>
+                </div>
+            </div>
+            <div class="list_of_second_div">
+                <div class="fc_image"id="support_image">
+                    <img src="./image/support-icon.png" alt="">
+                </div>
+                <div class="text">
+                       <h2>SUPPORT</h2>
+                    <p>24/7 SUPPORT</p>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+
+    <section class="p_page">
+         <div class="products_page">
+            <h1>FEATURED PRODUCTS</h1>
+            <p>Our Products..</p>  
+        </div>
+
+
+
+
+
+    </section>  
